82f23f1d7b4d9dab3dee82e426d104e6b57e7609
Badar Wildani <wildanie12@gmail.com>
2023-02-25 23:37:17 +0700

    feat: navbar toggler mechanism

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 // tulis kode javascript yang dibutuhkan disini ...
+const togglerEl = document.querySelector('#hamburger-menu')
+const navbarEl = document.querySelector('.navbar .navbar-nav')
+togglerEl.addEventListener('click', () => {
+    navbarEl.classList.toggle('active')
+})
+
+document.addEventListener('click', (e) => {
+    if (!togglerEl.contains(e.target) && !navbarEl.contains(e.target)) {
+        navbarEl.classList.remove('active')
+    }
+})
